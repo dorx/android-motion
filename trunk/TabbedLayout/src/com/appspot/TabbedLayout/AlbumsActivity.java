5c65a7d4b0273b5c480149043d6df4710c8d0b1f
alexfandrianto@gmail.com <alexfandrianto@gmail.com@2efd9bad-f0d3-56a8-3907-1b358e319faa>
2011-03-02 08:37:52 +0000

    TabbedLayout and Maps show up.
    I implemented automatic updates and made a single (low-quality) tab icon.

---
+++ trunk/TabbedLayout/src/com/appspot/TabbedLayout/AlbumsActivity.java
@@ -0,0 +1,16 @@
+package com.appspot.TabbedLayout;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.widget.TextView;
+
+public class AlbumsActivity extends Activity {
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.temp);
+
+        /*TextView textview = new TextView(this);
+        textview.setText("This is the Albums tab");
+        setContentView(textview);*/
+    }
+}
---
+++ trunk/TabbedLayout/src/com/appspot/TabbedLayout/SongsActivity.java
@@ -0,0 +1,27 @@
+package com.appspot.TabbedLayout;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+public class SongsActivity extends Activity {
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.calories);
+
+        /*TextView textview = new TextView(this);
+        textview.setText("This is the Songs tab");
+        setContentView(textview);*/
+    }
+    
+    public void launchSettings(View view) {
+        
+    }    
+    
+    public void logout(View view) {
+    
+    }
+    
+    
+}
