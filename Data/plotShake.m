d3b94256e75df1427c13386c98b84baeaff3e73b
alexfandrianto@gmail.com <alexfandrianto@gmail.com@2efd9bad-f0d3-56a8-3907-1b358e319faa>
2011-03-04 05:25:14 +0000

    My walking data. I stopped at a light for a short period.

--- Data/plotShake.m
+++ Data/plotShake.m
@@ -3,21 +3,27 @@
 %D = load('B/sevendesktwo.acsn'); % not very impressive
 %D = load('G/walking_8_mnf.acsn'); % looks nice 
 %D = load('morning/morning_2.acsn');
-L = size(D,1);
+D = load('C:\Users\AlexFandrianto\Desktop\Articles\CS141b\02_11_11_alex_walk.acsn');
+E = D;
+for i=1:60
+    D = E(1000*(i-1)+1:1000*i, :);
+    L = size(D,1);
 
-x = D(:,1);
-y = D(:,2);
-z = D(:,3);
-t = D(:,4);
+    x = D(:,1);
+    y = D(:,2);
+    z = D(:,3);
+    t = D(:,4);
 
-Fs = 80; %Hz
-NFFT = 2^nextpow2(L); % Next power of 2 from length of y
-Y = fft(z,NFFT)/L;
-f = Fs/2*linspace(0,1,NFFT/2+1);
+    Fs = 80; %Hz
+    NFFT = 100; %2^nextpow2(L); % Next power of 2 from length of y
+    Y = fft(z,NFFT)/L;
+    f = Fs/2*linspace(0,1,NFFT/2+1);
 
-% Plot single-sided amplitude spectrum.
-plot(f,2*abs(Y(1:NFFT/2+1))) 
-axis([0 2.5 0 0.025])
-title('Single-Sided Amplitude Spectrum, 9 desk y')
-xlabel('Frequency (Hz)')
-ylabel('|Y(f)|')
+    % Plot single-sided amplitude spectrum.
+    plot(f,2*abs(Y(1:NFFT/2+1))) 
+    hold on
+    %axis([0 2.5 0 0.025])
+    title('Single-Sided Amplitude Spectrum, 9 desk y')
+    xlabel('Frequency (Hz)')
+    ylabel('|Y(f)|')
+end
